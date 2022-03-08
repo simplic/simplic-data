834dba11485369d003784e93202bd72f445b4ce1
Tobias <tobias.milde@simplic.biz>
2022-03-08 13:59:11 +0100

    Adds IModel interface.

---
+++ src/Simplic.Data/IModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Simplic.Data
+{
+    /// <summary>
+    /// Basic interfacce for models with a generic type id.
+    /// </summary>
+    /// <typeparam name="TId"></typeparam>
+    public interface IModel<TId>
+    {
+        /// <summary>
+        /// Identifer of the model.
+        /// </summary>
+        TId Id { get; set; }
+    }
+}
